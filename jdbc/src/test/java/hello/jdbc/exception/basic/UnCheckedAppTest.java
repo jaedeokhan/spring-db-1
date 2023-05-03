74d73cb9601a7baa7efd8b649c48d252ccee5fa8
jaedeokhan <hjaedeok@gmail.com>
2023-05-03 19:57:59 +0900

    ADD : 예외 포함과 스택 트레이스 - 체크 예외에서 언체크(런타임) 예외로 전환할 때는 기존 예외를 꼭 포함! :#

--- jdbc/src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
+++ jdbc/src/test/java/hello/jdbc/exception/basic/UnCheckedAppTest.java
@@ -1,11 +1,13 @@
 package hello.jdbc.exception.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.net.ConnectException;
 import java.sql.SQLException;
 
+@Slf4j
 public class UnCheckedAppTest {
 
     @Test
@@ -14,6 +16,17 @@ public class UnCheckedAppTest {
         Assertions.assertThatThrownBy(() -> controller.request())
                 .isInstanceOf(RuntimeSQLException.class);
     }
+
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+        try {
+            controller.request();
+        } catch (Exception e) {
+            log.info("ex", e);
+        }
+
+  }
 
     static class Controller {
         Service service = new Service();
@@ -61,6 +74,9 @@ public class UnCheckedAppTest {
     }
 
     static class RuntimeSQLException extends RuntimeException {
+        public RuntimeSQLException() {
+        }
+
         public RuntimeSQLException(Throwable cause) {
             super(cause);
         }
